8febad0e1e94bdb60ce61d69247dce3596b066c9
O0OMichael <754105906@qq.com>
2024-05-01 17:47:10 -0700

    log in and log out

--- frontend/src/dialog/talk.jsx
+++ frontend/src/dialog/talk.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { TextField, Button, Card, CardMedia, CardContent, Typography, Grid } from '@mui/material';
 import CityList from '../cityList/CityList';
+import LoginPage from '../logIn/logIn';
+import SignUpPage from '../signUp/signUp';
 
 function MyFormComponent() {
     const dialogStyle = {
@@ -9,23 +11,22 @@ function MyFormComponent() {
         justifyContent: 'center',
         alignItems: 'center',
         maxWidth: '1000px',
-        paddingTop: '100px', // 顶部留白，根据需要调整
+        paddingTop: '100px',
     };
 
-    const [inputValue, setInputValue] = useState('');  // 状态管理输入值
-    const [data, setData] = useState([]); // 状态用于存储从服务器返回的数据
+    const [inputValue, setInputValue] = useState('');
+    const [data, setData] = useState([]);
 
     const handleSubmit = async (event) => {
-        event.preventDefault();  // 阻止表单默认提交行为
+        event.preventDefault();
         console.log('Sending data to backend:', inputValue);
-        // 这里使用 fetch 发送数据到后端端口
         try {
             const response = await fetch('http://localhost:3001/Search', {
-                method: 'POST',   // 或 'GET'，取决于你的后端需求
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ data: inputValue })  // 将输入数据作为 JSON 发送
+                body: JSON.stringify({ data: inputValue })
             });
             console.log('Input value:', inputValue);
             const responseData = await response.json();
@@ -37,66 +38,78 @@ function MyFormComponent() {
     };
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div style={{ width: '70%', display: 'flex', justifyContent: 'center', justifyContent: 'flex-start', marginTop: '10vh', marginLeft:'10vh' }}>
-                <form onSubmit={handleSubmit} style={{ width: '80%' }}> {/* 调整内部宽度为80%以进一步居中 */}
-                    <TextField
-                        multiline
-                        rows={4}
-                        label="Type something"
-                        variant="outlined"
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                        fullWidth
-                    />
-                    <Button type="submit" variant="contained" color="primary">
-                        Submit
-                    </Button>
-                    {data.length > 0 ? (
-                <Grid container spacing={2}>
-                    {data.map((item, index) => (
-                        <Grid item key={index} xs={12} sm={6} md={4}>
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardMedia
-                                    component="img"
-                                    height="140"
-                                    image={item.photos}
-                                    alt="Property Image"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {item.address}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Bedrooms: {item.bedrooms}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Bathrooms: {item.bathrooms}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Living Area: {item.livingArea} sqft
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Price: ${item.price}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Address: {item.address}, {item.city}, {item.state}, {item.zipcode}
+        <div>
 
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            ) : (
-                <Typography variant="h6" color="text.secondary" textAlign="center" sx={{ mt: 2 }}>
-                    No Result Found
-                </Typography>
-            )}
-                </form>
-            </div>
-            <div style={{ position: 'fixed', right: 0, top: 0, width: '30%', height: '100vh', overflowY: 'auto' }}>
-                <CityList />
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+
+                <div style={{ width: '70%', display: 'flex', justifyContent: 'center', justifyContent: 'flex-start', marginTop: '5vh', marginLeft: '10vh' }}>
+                    <div>
+                        <div style={{ display: 'flex', flexDirection: 'row' }}>
+                            <LoginPage/>
+                            <SignUpPage />
+                        </div>
+
+
+                        <form onSubmit={handleSubmit} style={{ width: '80%' }}>
+                            <TextField
+                                multiline
+                                rows={4}
+                                label="Type something"
+                                variant="outlined"
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                                fullWidth
+                            />
+                            <Button type="submit" variant="contained" color="primary">
+                                Submit
+                            </Button>
+                            {data.length > 0 ? (
+                                <Grid container spacing={2}>
+                                    {data.map((item, index) => (
+                                        <Grid item key={index} xs={12} sm={6} md={4}>
+                                            <Card sx={{ maxWidth: 345 }}>
+                                                <CardMedia
+                                                    component="img"
+                                                    height="140"
+                                                    image={item.photos}
+                                                    alt="Property Image"
+                                                />
+                                                <CardContent>
+                                                    <Typography gutterBottom variant="h5" component="div">
+                                                        {item.address}
+                                                    </Typography>
+                                                    <Typography variant="body2" color="text.secondary">
+                                                        Bedrooms: {item.bedrooms}
+                                                    </Typography>
+                                                    <Typography variant="body2" color="text.secondary">
+                                                        Bathrooms: {item.bathrooms}
+                                                    </Typography>
+                                                    <Typography variant="body2" color="text.secondary">
+                                                        Living Area: {item.livingArea} sqft
+                                                    </Typography>
+                                                    <Typography variant="body2" color="text.secondary">
+                                                        Price: ${item.price}
+                                                    </Typography>
+                                                    <Typography variant="body2" color="text.secondary">
+                                                        Address: {item.address}, {item.city}, {item.state}, {item.zipcode}
+
+                                                    </Typography>
+                                                </CardContent>
+                                            </Card>
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            ) : (
+                                <Typography variant="h6" color="text.secondary" textAlign="center" sx={{ mt: 2 }}>
+                                    No Result Found
+                                </Typography>
+                            )}
+                        </form>
+                    </div>
+                </div>
+                <div style={{ position: 'fixed', right: 0, top: 0, width: '30%', height: '100vh', overflowY: 'auto' }}>
+                    <CityList />
+                </div>
             </div>
         </div>
     );
---
+++ frontend/src/signUp/signUp.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+
+function SignUpPage() {
+    const [open, setOpen] = useState(false); // 控制对话框开关
+    const [username, setUsername] = useState(''); // 用户名
+    const [password, setPassword] = useState(''); // 密码
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSignUp = () => {
+        console.log('Username:', username, 'Password:', password);
+        // 这里可以添加登录逻辑
+        handleClose(); // 登录后关闭对话框
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'center', marginBottom:'5vh'}}>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                Sign Up
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Sign Up</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please enter your username and password.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="username"
+                        label="Username"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="password"
+                        label="Password"
+                        type="password"
+                        fullWidth
+                        variant="standard"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSignUp} color="primary">
+                        Log in
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default SignUpPage;
